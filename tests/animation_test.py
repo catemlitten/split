3a52a9a1d205d0d51d3c166a08487e16a684f925
catemlitten <clitten@luc.edu>
2018-02-18 19:48:39 -0600

    Board and players set by level texts now.

--- tests/animation_test.py
+++ tests/animation_test.py
@@ -25,15 +25,15 @@ def main():
 
     direction = '-'
     board = Board()
-    tiles = board.getTiles("level1.txt");
+    tiles = board.getTiles("level2.txt");
     #board object can be created here, and the parameter for the __init__ can be the level file at [path + '/levels/level1.txt']
     #it can have check() to check if jumps are possible
     #update() to edit tiles after a jump
     #and getPlayerInitPos() so we can put the players in the right position initially. 
         #Maybe this can be an array like this [[9,6],[12,11]]
 
-    p1 = Player(path + '/animation/character1/', 9, 6)
-    p2 = Player(path + '/animation/character2/', 12, 11)
+    p1 = Player(path + '/animation/character1/', board.player1[0], board.player1[1])
+    p2 = Player(path + '/animation/character2/', board.player2[0], board.player2[1])
 
     #Maybe this animation objects can be somehow part of the board? Think about later
     anims = [	
--- tests/board.py
+++ tests/board.py
@@ -8,40 +8,46 @@ class Board:
         self.red_coin = '/animation/coin_red.png'
         self.blue_coin = '/animation/coin_blue.png'
         self.tiles = []
+        self.player1 = [0, 0]
+        self.player2 = [0, 0]
 
     def read_level(self, level_file):
         fileHand = open(level_file, 'r')
         y = 1
         x = 1
         for line in fileHand:
-            for letter in line:
-                print(letter)
-            whatGoesThere = 't'
-            print(whatGoesThere)
-            if(whatGoesThere == 'e'):
-                # nothing to make, but increment x
-                x += 1
-            elif(whatGoesThere == 't'):
-                self.tiles.append(Tile(self.coin, x, y))
-                x += 1
-            elif(whatGoesThere == 'w'):
-                self.tiles.append(Tile(self.thymbal, x, y))
-                x += 1
-            elif(whatGoesThere == 'q'):
-                self.tiles.append(Tile(self.red_coin, x, y))
-                x += 1
-            elif(whatGoesThere == 'p'):
-                self.tiles.append(Tile(self.blue_coin, x, y))
-                x += 1
-            elif(whatGoesThere == '1'):
-                self.tiles.append(Tile(self.coin, x, y))
-                x += 1
-            elif(whatGoesThere == '2'):
-                self.tiles.append(Tile(self.coin, x, y))
-                x += 1
-            else:
-                print("We have an unknown identifier in our level...")
+            for whatGoesThere in line:
+                if(whatGoesThere == 'e'):
+                    # nothing to make, but increment x
+                    x += 1
+                elif(whatGoesThere == 't'):
+                    self.tiles.append(Tile(self.coin, x, y))
+                    x += 1
+                elif(whatGoesThere == 'w'):
+                    self.tiles.append(Tile(self.thymbal, x, y))
+                    x += 1
+                elif(whatGoesThere == 'q'):
+                    self.tiles.append(Tile(self.red_coin, x, y))
+                    x += 1
+                elif(whatGoesThere == 'p'):
+                    self.tiles.append(Tile(self.blue_coin, x, y))
+                    x += 1
+                elif(whatGoesThere == '1'):
+                    self.player1[0] = x
+                    self.player1[1] = y
+                    print(self.player1)
+                    self.tiles.append(Tile(self.coin, x, y))
+                    x += 1
+                elif(whatGoesThere == '2'):
+                    self.player2[0] = x
+                    self.player2[1] = y
+                    self.tiles.append(Tile(self.coin, x, y))
+                    x += 1
+                else:
+                    print("Blank space? Why? strip() does not work")
             y += 1 # increment y access
+            x = 1 # reset x
+                # print(x,y)
 
     #Of course, this list won't be hard-coded, and will instead come from board.getTiles()
     '''
