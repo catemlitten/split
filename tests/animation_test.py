653942b8408d57e54c44b9dc88e304b0ad03bf87
catemlitten <clitten@luc.edu>
2018-02-23 12:55:40 -0600

    Preliminary code for tracking where player is on board.

--- tests/animation_test.py
+++ tests/animation_test.py
@@ -26,18 +26,21 @@ def main():
 
     direction = '-'
     board = Board()
-    tiles = board.getTiles("level2.txt");
-    #board object can be created here, and the parameter for the __init__ can be the level file at [path + '/levels/level1.txt']
-    #it can have check() to check if jumps are possible
-    #update() to edit tiles after a jump
-    #and getPlayerInitPos() so we can put the players in the right position initially. 
+    tiles = board.get_tiles("level2.txt");
+    '''
+    board object can be created here, and the parameter for the __init__ can be the level file at [path + '/levels/level1.txt']
+    it can have check() to check if jumps are possible
+    update() to edit tiles after a jump
+    and getPlayerInitPos() so we can put the players in the right position initially. 
         #Maybe this can be an array like this [[9,6],[12,11]]
+    '''
 
     p1 = Player(path + '/animation/character1/', board.player1[0], board.player1[1])
     p2 = Player(path + '/animation/character2/', board.player2[0], board.player2[1])
-
-    #Maybe this animation objects can be somehow part of the board? Think about later
-    # maybe make them randomly chosen from empty spaces
+    '''
+    Maybe this animation objects can be somehow part of the board? Think about later
+     maybe make them randomly chosen from empty spaces
+    '''
     emptySpots = board.emptySpots
     animationSpots = []
     for x in range(4):
@@ -75,13 +78,12 @@ def main():
         for i in range(len(tiles)):
             screen.blit(get_image(path + tiles[i].path), tiles[i].getRealXY())
        
-        p1.update(direction, screen) #add board parameter to check if jump is possible
-        p2.update(direction, screen)
+        p1.update(direction, screen, board.board)  # add board parameter to check if jump is possible
+        p2.update(direction, screen, board.board)
 
         for i in range(len(anims)):
             anims[i].update(screen)
 
-        
         pygame.display.flip()
         clock.tick(60)
 
